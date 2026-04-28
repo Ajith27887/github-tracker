df4e5c899f04cf5940a068ae10be3ba466aca6ed
Ajith27887 <ajithyogesh.n@gmail.com>
2026-04-28 08:30:56 +0530

    proxy added

--- server/server.js
+++ server/server.js
@@ -39,6 +39,7 @@ app.use(session({
 	secret: process.env.SESSION_SECRET,
 	resave: false,
 	saveUninitialized: false,
+	proxy: true,
 	cookie: {
 		secure: process.env.NODE_ENV === 'production',
 		sameSite: 'none'
